df167442e16e5874a3e039f78c2f94b4db43f9b2
deleitempanadas <deleite.empanadas2012@gmail.com>
2022-01-26 20:26:05 -0600

    third commit

--- js/index.js
+++ js/index.js
@@ -81,23 +81,9 @@ window.sr = ScrollReveal();
 
 
 //CARRUSEL
-            $(document).ready(function(){
-            $("#mycarousel").carousel( { interval: 3000 } );
-            $("#carouselButton").click(function(){
-                if ($('#carouselButton').children('span').hasClass('fa-pause')){
-                    $("#mycarousel").carousel('pause');
-                    $('#carouselButton').children('span').removeClass('fa-pause');
-                    $('#carouselButton').children('span').addClass('fa-play');
+           
 
-                }
-                else  if ($('#carouselButton').children('span').hasClass('fa-play')){
-                    $("#mycarousel").carousel('cycle');
-                    $('#carouselButton').children('span').removeClass('fa-play');
-                    $('#carouselButton').children('span').addClass('fa-pause');
 
-                }
-            });
-        });
 
 $(document).ready(function () {
   $(window).scroll(function () {
